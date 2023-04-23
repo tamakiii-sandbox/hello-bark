ce5c7ef87ac5e7e709bf97489456633061ee0e62
tamakiii <tamakiii@users.noreply.github.com>
2023-04-23 16:27:18 +0900

    fixes

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+import torch
 from bark import SAMPLE_RATE, generate_audio, preload_models
 from IPython.display import Audio
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if device.type == 'cuda':
+    print(f'Using GPU: {torch.cuda.get_device_name(0)}')
+else:
+    print('No GPU available. Using CPU.')
+
 preload_models()
 
-text_prompt = """"
-	This article, titled "Merging domain events before dispatching" and published on June 6, 2019,
-	discusses how to handle multiple domain events when raising one of them should negate the others.
-	Domain events are significant events in a specific domain and involve event producers, event consumers,
-	and the event dispatcher.
+text_prompt = """
+	Hello.
 """
 
-audio_array = generate_audio(text_promt)
+# text_prompt = """"
+# 	This article, titled "Merging domain events before dispatching" and published on June 6, 2019,
+# 	discusses how to handle multiple domain events when raising one of them should negate the others.
+# 	Domain events are significant events in a specific domain and involve event producers, event consumers,
+# 	and the event dispatcher.
+# """
+
+audio_array = generate_audio(text_prompt)
 
 Audio(audio_array, rate=SAMPLE_RATE)
